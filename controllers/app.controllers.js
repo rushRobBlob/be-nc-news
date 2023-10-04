2e1193a4044912da7ad4f1af649402d5fdfb8b52
rushRobBlob <robbie.rush@ymail.com>
2023-10-04 12:28:43 +0100

    completed ticket 6 GET comments by article_id

--- controllers/app.controllers.js
+++ controllers/app.controllers.js
@@ -22,5 +22,10 @@ exports.getArticlesById = (req, res, next) => {
 exports.getCommentsByArticleId = (req, res, next) => {
     const { article_id: id } = req.params;
 
-    retrieveCommentsByArticleId(id);
+    retrieveCommentsByArticleId(id).then((comments) => {
+        res.status(200).send({ comments });
+    })
+        .catch((err) => {
+            next(err);
+        });
 }
